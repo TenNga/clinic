a4cc1d1d05487deec23c29fd712136862373e735
Kevin Karma <tenngawang6080@gmail.com>
2024-10-19 14:55:19 -0400

    creating appointment form

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
-const NewAppointment = () => {
+export default function NewAppointment() {
   return (
-    <div>NewAppointment</div>
-  )
-}
+  <div className="flex h-screen max-h-screen">
+    {/* TODO : OTP verification */}
+    <section className="remove-scrollbar container my-auto">
+      <div className="sub-container max-w-[860px] flex-1 justify-between">
+        <Image 
+        src="/assets/icons/logo-full.svg" 
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="mb-12 h-10 w-fit"
+        />
 
-export default NewAppointment
+        <AppointmentForm />
+
+        <p className="justify-items-end text-dark-600 xl:text-left">
+            © 2024 Carepluse
+        </p>
+      </div>
+    </section>
+
+    <Image
+      src="/assets/images/appointment-img.png"
+      height={1000}
+      width={1000}
+      alt="appointment"
+      className="side-img max-w-[390px] bg-bottom"
+    >
+
+    </Image>
+  </div>
+  );
+}
